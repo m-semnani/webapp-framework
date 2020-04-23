073a51781a258f0eb4ef8fe3187661cf68abc42e
mohammad.semnani <mohammad.semnani@linkit.nl>
2020-04-23 22:20:25 +0200

    validation added to login form.

--- front-angular/src/app/login/login.component.ts
+++ front-angular/src/app/login/login.component.ts
@@ -1,38 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../service/authentication.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
-selector: 'app-login',
-templateUrl: './login.component.html',
-styleUrls: ['./login.component.css']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-username = ''
-password = ''
-invalidLogin = false
-errmsg = ''
+  username = ''
+  password = ''
+  invalidLogin = false
+  errMsg = ''
 
-constructor(private router: Router,
-    private loginservice: AuthenticationService) { }
+  constructor(private router: Router, private loginservice: AuthenticationService) { }
 
   ngOnInit() {
   }
 
-  checkLogin() {
-    (this.loginservice.authenticate(this.username, this.password).subscribe(
+  loginUser(regForm: NgForm) {
+    (this.loginservice.authenticate(regForm.value.username, regForm.value.password).subscribe(
       data => {
         this.router.navigate([''])
         this.invalidLogin = false
       },
       error => {
         this.invalidLogin = true
-        this.errmsg = 'username or password is wrong!'
+        this.errMsg = 'username or password is wrong!'
       }
-)
-);
-
-}
-
+    )
+    );
+  }
 }
